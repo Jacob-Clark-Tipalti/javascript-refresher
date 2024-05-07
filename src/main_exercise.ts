50a7102c9d2926a8bc718cccfa2460e2740e3ea9
Jacob Clark <jacob.clark@UK-JCLARK.local>
2024-05-07 17:03:16 +0100

    Main Exercise

---
+++ src/main_exercise.ts
@@ -0,0 +1,71 @@
+function fizzBuzz(): void {
+    for (let curr = 1; curr <= 100; curr += 1) {
+        let toPrint = '';
+        if (curr % 3 === 0) {
+            toPrint = newToPrintForMultipleOf3(toPrint);
+        }
+        
+        if (curr % 5 === 0) {
+            toPrint = newToPrintForMultipleOf5(toPrint);
+        }
+
+        if (curr % 7 === 0) {
+            toPrint = newToPrintForMultipleOf7(toPrint);
+        }
+        
+        if (curr % 11 === 0) {
+            toPrint = newToPrintForMultipleOf11();
+            console.log(toPrint);
+            continue;
+        }
+
+        if (curr % 13 === 0) {
+            toPrint = newToPrintForMultipleOf13(toPrint);
+        }
+
+        if (curr % 17 === 0) {
+            toPrint = newToPrintForMultipleOf17(toPrint);
+        }
+
+        if (toPrint === '') {
+            toPrint = String(curr);
+        }
+
+        console.log(toPrint);
+    }
+}
+
+function newToPrintForMultipleOf3(prevToPrint: string): string {
+    return prevToPrint + 'Fizz';
+}
+
+function newToPrintForMultipleOf5(prevToPrint: string): string {
+    return prevToPrint + 'Buzz';
+}
+
+function newToPrintForMultipleOf7(prevToPrint: string): string {
+    return prevToPrint + 'Bang';
+}
+
+function newToPrintForMultipleOf11(): string {
+    return 'Fezz';
+}
+
+function newToPrintForMultipleOf13(prevToPrint: string): string {
+    let indexOfB = prevToPrint.indexOf('B');
+        if (indexOfB === -1) {
+            return prevToPrint + 'Bong';
+        } else {
+            return prevToPrint.slice(0, indexOfB) + 'Bong' + prevToPrint.slice(indexOfB);
+        }
+}
+
+function newToPrintForMultipleOf17(toPrint: string): string {
+    toPrint = toPrint.replace(/([A-Z])/g, ' $1').trim();
+    let toPrintWords = toPrint.split(' ');
+    return toPrintWords.reverse().join('');
+}
+
+fizzBuzz();
+console.log('Multiple of 17 function check. Expected output: BongFizzBuzzFuzzFezz');
+console.log(newToPrintForMultipleOf17('FezzFuzzBuzzFizzBong'));
